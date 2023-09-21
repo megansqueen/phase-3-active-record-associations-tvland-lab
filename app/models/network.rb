14e0791e3063a3be885067e33e9ba6ba9643fa54
Megan <esler6724@gmail.com>
2023-09-20 18:09:25 -0700

    done

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,10 @@
 class Network < ActiveRecord::Base
-  
+  has_many :shows
+  has_many :actors
+  has_many :characters
+
+    def sorry
+        "We're sorry about passing on John Mulaney's pilot"
+    end
+
 end
